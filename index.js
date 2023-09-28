44a75c5ba9cc15186174ae0fea836d33aadf32b7
Varun Bhatt <vbhatt75@gmail.com>
2023-09-28 19:48:36 -0400

    This is last Commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let SERVER_NAME = 'product-api'
-let PORT =5000;
+let PORT =4000;
 let HOST = '127.0.0.1';
 
 let errors = require('restify-errors');
@@ -166,7 +166,7 @@ server.del('/products/:id', function (req, res, next) {
 
 
 
-// Delete all products in the persistence engine
+///Delete all products in the persistence engine
 server.del('/products', function (req, res, next) {
     console.log('POST /users params=>' + JSON.stringify(req.params));
     // Delete the all products with the persistence engine
@@ -174,8 +174,9 @@ server.del('/products', function (req, res, next) {
     productSave.deleteMany({}, function (error, products) {
 
         // Return all of the users in the system
-        res.send(201,"All records deleted")
         console.log("Records deleted");
+        res.send(201,"All records deleted")
+        
       })
   
   })
